8d03c244b973028286bbc436144ea689fce7f259
Md-Gajjali <hellogajjali@gmail.com>
2026-01-21 13:46:25 +0600

    update

--- src/Componets/Banner.jsx
+++ src/Componets/Banner.jsx
@@ -11,10 +11,10 @@ const Banner = () => {
             <h4 className='text-white text-[17px]'>Hello, I'm</h4>
             <h1 className='text-5xl text-DarkBlue'>Emam Gajjali</h1>
             <p className='text-[19px] text-primary '>Frontend Developer</p>
-            <div>
-                <Buttons >Download CV</Buttons>
-                <Buttons >About</Buttons>
-            </div>
+            <Flex className='gap-10 justify-center mt-10'>
+                <Buttons className='text-DarkBlue  border hover:bg-DarkBlue hover:text-black linear ease-in-out duration-400 '>Download CV</Buttons>
+                <Buttons className='bg-DarkBlue text-black '>About</Buttons>
+            </Flex>
         </div>
       </Container>
     </div>
